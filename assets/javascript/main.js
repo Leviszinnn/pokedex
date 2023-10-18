cd66612d4beb7954f51cad4d2f36f1ac950a545b
LevlzlnnnDev <lbds.bueno@gmail.com>
2023-10-17 23:11:12 -0300

    tipagem dos pokemons coloridas

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,23 @@
-console.log("sucesso")
+function convertPokemonToLI(pokemon) {
+    return `
+    <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
+                    <img src="${pokemon.photo}"
+                    alt="${pokemon.name}" />
+                </div>
+            </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+PokeAPI.getPokemons().then((pokemons = []) => {
+    const newHTML = pokemons.map(convertPokemonToLI).join('')
+    pokemonList.innerHTML += newHTML
+})
+
